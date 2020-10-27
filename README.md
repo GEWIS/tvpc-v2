f2e7ae8e7471decd5061f9d07528a4af2447f7a2
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2020-10-27 15:32:40 +0100

    Initial commit (hello world)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tvpc-v2
-Complete rewrite of the TVPC frontend of Study Association GEWIS
+
+# Typescript module
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const doc = document.getElementsByTagName('h1')[0];
+doc.innerHTML = 'Hello World, but from TypeScript!';
